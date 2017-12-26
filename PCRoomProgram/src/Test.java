f0133781b2cbf935a0e03f850482185cd2151224
Jean-Gyu Hu <gjwlsrb1700@gmail.com>
2017-12-26 18:33:11 +0900

    masterererer test

--- PCRoomProgram/src/Test.java
+++ PCRoomProgram/src/Test.java
@@ -17,8 +17,7 @@ public class Test {
 		System.out.println("TEST");
 		System.out.println("Test");
 		System.out.println("Test");
-		System.out.println("Test");
-		System.out.println("Test");
+
 		
 		
 	}
