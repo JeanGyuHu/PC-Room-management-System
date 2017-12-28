71bbdcf6da4ea018724f5b435ef6507fc1b9991f
민우 <@kimminwoo>
2017-12-28 18:44:24 +0900

    클라이언트 로그인후 화면

--- PCRoomClient/src/Test.java
+++ PCRoomClient/src/Test.java
@@ -4,7 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
 		
+		
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
