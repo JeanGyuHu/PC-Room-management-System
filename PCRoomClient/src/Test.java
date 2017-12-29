f8453ad54612f8da99557f062b0c0c704fda6c19
user <user@samsung>
2017-12-29 15:23:51 +0900

    ㅔㅣ

--- PCRoomClient/src/Test.java
+++ PCRoomClient/src/Test.java
@@ -4,7 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		LoginPanel login = new LoginPanel();
+		frame.getContentPane().add(login);
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
